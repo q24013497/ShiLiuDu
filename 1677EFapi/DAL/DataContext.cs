493b95e86c761d9c4d2f87141704bc262ce45440
luojianing <54881363+luojianing@users.noreply.github.com>
2019-09-05 14:18:39 +0800

    上下文

--- 1677EFapi/DAL/DataContext.cs
+++ 1677EFapi/DAL/DataContext.cs
@@ -18,7 +18,28 @@ namespace DAL
         {
 
         }
-        public DbSet<Users> UserModelset { get; set; }
+        public DbSet<ApplyFor> ApplyFor { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<Collect> Collect { get; set; }
+        public DbSet<CommodityDetails> CommodityDetails { get; set; }
+        public DbSet<DetailInformation> DetailInformation { get; set; }
+        public DbSet<Evaluate> Evaluate { get; set; }
+        public DbSet<Examine> Examine { get; set; }
+        public DbSet<HotSearch> HotSearch { get; set; }
+        public DbSet<Information> Information { get; set; }
+        public DbSet<InformationType> InformationType { get; set; }
+        public DbSet<Master> Master { get; set; }
+        public DbSet<MyBalance> MyBalance { get; set; }
+        public DbSet<MyIntegral> MyIntegral { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<ProductIntroduction> ProductIntroduction { get; set; }
+        public DbSet<Region> Region { get; set; }
+        public DbSet<Sale> Sale { get; set; }
+        public DbSet<Shop> Shop { get; set; }
+        public DbSet<Trade> Trade { get; set; }
+        public DbSet<UserInfo> UserInfo { get; set; }
+        public DbSet<VisitorsTApplay> VisitorsTApplay { get; set; }
+        public DbSet<Model.Type> Type { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
